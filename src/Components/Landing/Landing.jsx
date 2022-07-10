196850cff160ad9b9fcba7e530b69f2b990c3516
Spider9480 <omgawandeofficial9834899149@gmail.com>
2022-07-10 06:22:19 +0530

    homepage structure done

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -1,7 +1,84 @@
 import React from "react";
+import classes from "./Landing.module.css";
+import spider from "../../Assets/spider.png";
+import harsh from "../../Assets/harsh.png";
+import sapna from "../../Assets/sapna.png";
+import a from "../../Assets/1.png";
+
+const creators = [
+  {
+    img: spider,
+    name: "Om Gawande",
+  },
+  {
+    img: harsh,
+    name: "Harsh Jain",
+  },
+  {
+    img: sapna,
+    name: "Sapna Kul",
+  },
+];
+
+const Creator = () => {
+  return creators.map((creator, key) => {
+    return (
+      <>
+        <div key={key} className="creators_container">
+          <div className={classes.creator_container}>
+            <div className={classes.creator_img_container}>
+              <img
+                alt="creator"
+                className={classes.creator_img}
+                src={creator.img}
+              />
+            </div>
+            <h1 className={classes.creator_name}>{creator.name}</h1>
+          </div>
+        </div>
+      </>
+    );
+  });
+};
 
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <>
+      <div className={classes.container}>
+        <div className={classes.left_container}>
+          <div className={classes.title}>
+            <h1 className={classes.title_text}>
+              <span className={classes.title_red}>Y</span>ouTube
+            </h1>
+            <h1 className={classes.title_text}>
+              <span className={classes.title_red}>T</span>imeline
+            </h1>
+            <h1 className={classes.title_text}>
+              <span className={classes.title_red}>B</span>ookmarker
+            </h1>
+            <div className={classes.dash}></div>
+          </div>
+          <div className={classes.description_container}>
+            <p className={classes.description_text}>
+              Bookmark important timelines in a youtube video, so that you never
+              have to hustle to find out the one event you are looking for when
+              you are revisting the video.
+            </p>
+          </div>
+          <div className={classes.btn_container}>
+            <button className={classes.btn}>Get Started &rarr;</button>
+          </div>
+          <div className={classes.creator_section}>
+            <h1 className={classes.creators_heading}>Creators</h1>
+            <Creator />
+          </div>
+        </div>
+        <div className={classes.right_container}>
+          <img src={a} alt="" className={classes.img1} />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Landing;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import logo_white from "../../Assets/logo_white.png";
+import github from "../../Assets/github.png";
+import classes from "./Navbar.module.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <>
+      <nav className={classes.navbar}>
+        <div className={classes.img_container}>
+          <img src={logo_white} alt="" className={classes.img} />
+        </div>
+        <div className={classes.nav_links_container}>
+          <div className={classes.nav_link_container}>HOME</div>
+          <div className={classes.nav_link_container}>ABOUT</div>
+          <div className={classes.nav_link_container}>EXTENSION</div>
+          <div className={classes.nav_link_container}>CONTACT US</div>
+        </div>
+        <div className={classes.github_container}>
+          <img src={github} alt="" className={classes.github_img} />
+        </div>
+      </nav>
+    </>
+  );
 };
 
 export default Navbar;

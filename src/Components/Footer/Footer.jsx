66d0427877c515163843d5d86507753dc76067f0
Spider9480 <omgawandeofficial9834899149@gmail.com>
2022-07-13 02:06:33 +0530

    footer done + responsive

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import classes from "./Footer.module.css";
+import logo from "../../Assets/logo_dark.png";
 
 const Footer = () => {
   return (
@@ -20,12 +21,44 @@ const Footer = () => {
           </div>
         </div>
         {/* Bottom */}
-        {/* <div className={classes.}>
-          <div className={classes.}></div>
-          <div className={classes.}></div>
-          <div className={classes.}></div>
-          <div className={classes.}></div>
-        </div> */}
+        <div className={classes.footer_bottom}>
+          <div className={classes.divider}>
+            <div className={classes.footer_copywrite}>
+              <img src={logo} alt="logo" className={classes.footer_logo} />
+              <h4>
+                <span className={classes.grey}>©</span> 2022
+              </h4>
+            </div>
+            <div className={classes.footer_bottom_content}>
+              <h2 className={classes.content_top}>Our Creators</h2>
+              <div>
+                <h1 className={classes.links}>Spider</h1>
+                <h1 className={classes.links}>Sriver</h1>
+                <h1 className={classes.links}>Dream</h1>
+              </div>
+            </div>
+          </div>
+          <div className={classes.divider}>
+            <div className={classes.footer_bottom_content}>
+              <h2 className={classes.content_top}>Links</h2>
+              <div>
+                <h1 className={classes.links}>Home</h1>
+                <h1 className={classes.links}>About Us</h1>
+                <h1 className={classes.links}>Extension</h1>
+              </div>
+            </div>
+            <div className={classes.footer_bottom_content}>
+              <h2 className={classes.content_top}>⭐️ Repos</h2>
+              <div>
+                <h1 className={classes.links}>Extension Page</h1>
+                <h1 className={classes.links}>Extension</h1>
+              </div>
+            </div>
+          </div>
+          <div></div>
+        </div>
+        <div className={classes.dash}></div>
+        <h4 className={classes.dash_content}>©2022 by YTTB</h4>
       </div>
     </>
   );

3cad62a434c41199e00d4312701cc09cf3c5bf38
Spider9480 <omgawandeofficial9834899149@gmail.com>
2022-07-13 01:15:57 +0530

    footer buymeacoffee added

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import classes from "./Footer.module.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <div className={classes.footer}>
+        <div className={classes.footer_top}>
+          <div className={classes.footer_content}>
+            <p className={classes.footer_content1}>
+              Help us publish this extension on Chrome!
+            </p>
+            <p className={classes.footer_content2}>Thank you !</p>
+          </div>
+          <div className={classes.footer_btn}>
+            <a href="https://www.buymeacoffee.com/omgawande">
+              <img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=omgawande&button_colour=fff&font_colour=000000&font_family=Cookie&outline_colour=000000&coffee_colour=282828" />
+            </a>
+            {/* <btn></btn> */}
+          </div>
+        </div>
+        {/* Bottom */}
+        {/* <div className={classes.}>
+          <div className={classes.}></div>
+          <div className={classes.}></div>
+          <div className={classes.}></div>
+          <div className={classes.}></div>
+        </div> */}
+      </div>
+    </>
+  );
 };
 
 export default Footer;
